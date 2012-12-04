5cdb057093c46cb500470644cd6287be128b765d
Judd Lillestrand <juddl333@gmail.com>
2012-12-04 13:02:47 -0800

    compile assets

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module Lvrug
     # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
     # config.sass.prefered_syntax = :sass
 
     # Version of your assets, change this if you want to expire all your assets
