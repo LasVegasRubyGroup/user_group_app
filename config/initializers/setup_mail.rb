d418ce56a5d4963f708bda98b82747770648dd48
Judd Lillestrand <juddl333@gmail.com>
2012-12-12 18:42:10 -0800

    comment out mail settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,15 +1,15 @@
-require 'tlsmail'    
-Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+# require 'tlsmail'    
+# Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.smtp_settings = {
-  :enable_starttls_auto => true,  
-  :address            => 'smtp.gmail.com',
-  :port               => 587,
-  :domain             => 'gmail.com', #you can also use google.com
-  :authentication     => "plain",
-  :user_name          => ENV['TOPICS_EMAIL_USERNAME'],
-  :password           => ENV['TOPICS_EMAIL_PASSWORD'] 
-}
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.perform_deliveries = true
+# ActionMailer::Base.raise_delivery_errors = true
+# ActionMailer::Base.smtp_settings = {
+#   :enable_starttls_auto => true,  
+#   :address            => 'smtp.gmail.com',
+#   :port               => 587,
+#   :domain             => 'gmail.com', #you can also use google.com
+#   :authentication     => "plain",
+#   :user_name          => ENV['TOPICS_EMAIL_USERNAME'],
+#   :password           => ENV['TOPICS_EMAIL_PASSWORD'] 
+# }
