e8a3e036626a7c3cdad7c6a5396dc8eb355224aa
Judd Lillestrand <juddl333@gmail.com>
2012-12-12 18:47:36 -0800

    another test at fixing asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,9 +16,11 @@ Lvrug::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
