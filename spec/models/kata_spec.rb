e02615dc716df66c29d2d1e37395390ba9dc8ed6
Gabriel Evans <gabriel@codeconcoction.com>
2013-01-23 19:26:54 -0800

    Add examples for multiple meetings and fix duplicate scope

--- spec/models/kata_spec.rb
+++ spec/models/kata_spec.rb
@@ -4,42 +4,74 @@ describe Kata do
 
   describe '#votes' do
 
-    context 'when there are no prior meetings' do
+    let(:presenter) do
+      create(:user)
+    end
 
-      let(:meeting) do
-        Meeting.prototype.tap do |meeting|
-          meeting.time_slots.each do |time_slot|
-            time_slot.presenter_id = presenter.id
+    subject(:kata) do
+      create(:kata)
+    end
+
+     let(:meeting) do
+      Meeting.prototype.tap do |meeting|
+        meeting.time_slots.each do |time_slot|
+          time_slot.presenter_id = presenter.id
+        end
+      end
+    end
+
+    let(:topic_1) do
+      create(:topic_with_votes, votes_count: 20)
+    end
+
+    let(:topic_2) do
+      create(:topic_with_votes, votes_count: 10)
+    end
+
+    before do
+      meeting.time_slots[0].topic_id = topic_1._id
+      meeting.time_slots[1].topic_id = topic_2._id
+      meeting.time_slots[2].topic_id = kata._id
+      meeting.save
+      kata.reload
+    end
+
+  context 'when there are no prior meetings' do
+
+      it 'returns the average of the other two topics' do
+        expect(kata.votes).to eq(15)
+      end
+    end
+
+    context 'when there are prior meetings' do
+
+      let!(:previous_meetings) do
+        3.times.collect do |i|
+          Meeting.prototype.tap do |meeting|
+            meeting.time_slots.each do |time_slot|
+              time_slot.presenter_id = presenter.id
+              time_slot.topic_id = send(:"topics_#{i}").pop.id
+            end
+            meeting.status = 'closed'
+            meeting.save
           end
         end
       end
 
-      let(:presenter) do
-        create(:user)
+      let(:topics_0) do
+        create_list(:topic_with_votes, 3, votes_count: 5)
       end
 
-      let(:topic_1) do
-        create(:topic_with_votes, votes_count: 20)
+      let(:topics_1) do
+        create_list(:topic_with_votes, 3, votes_count: 10)
       end
 
-      let(:topic_2) do
-        create(:topic_with_votes, votes_count: 10)
+      let(:topics_2) do
+        create_list(:topic_with_votes, 3, votes_count: 9)
       end
 
-      subject(:kata) do
-        create(:kata)
-      end
-
-      before do
-        meeting.time_slots[0].topic_id = topic_1._id
-        meeting.time_slots[1].topic_id = topic_2._id
-        meeting.time_slots[2].topic_id = kata._id
-        meeting.save
-        kata.reload
-      end
-
-      it 'returns the average of the other two topics' do
-        expect(kata.votes).to eq(15)
+      it 'returns the average of all of the topics' do
+        expect(kata.votes).to eq(9)
       end
     end
   end
