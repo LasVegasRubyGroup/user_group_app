551595e1d8b536d6f15cca812a53a7d610b63425
Judd Lillestrand <juddl333@gmail.com>
2013-02-21 09:57:29 -0800

    disable kata generation for now

--- app/models/kata_observer.rb
+++ app/models/kata_observer.rb
@@ -1,10 +1,10 @@
 class KataObserver < Mongoid::Observer
 
-  def after_save(_)
-    if Kata.open.count == 0
-      kata = Kata.generate
-      kata.save
-    end
-  end
+  # def after_save(_)
+  #   if Kata.open.count == 0
+  #     kata = Kata.generate
+  #     kata.save
+  #   end
+  # end
 
 end
